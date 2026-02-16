b8bbbad6bb511523af75f0a67ac97101c9757ca0
MelvinLMX <melvin.lee.mx@gmail.com>
2026-02-16 12:30:43 +0800

    added login action

--- DCOMs_Assignment/src/dcoms_assignment/Server.java
+++ DCOMs_Assignment/src/dcoms_assignment/Server.java
@@ -7,10 +7,24 @@ public class Server {
     public static void main(String[] args){
         try{
             Registry reg = LocateRegistry.createRegistry(1099); //creating a server under 1099
+            StaffService SS = new StaffService();
             
-            Identify Action1 = new IdentifyImpl();  //create object based on action class
+            if (SS.getAllStaff().isEmpty()) {
+
+                Staff hr = new Staff("HR01", "Alice", "HR", "1234");
+                Staff staff = new Staff("ST01", "Bob", "STAFF", "1234");
+
+                SS.AddStaff(hr);
+                SS.AddStaff(staff);
+
+                System.out.println("Default staff created.");
+            }
             
-            reg.rebind("SelfIdentify", Action1); 
+            Login LoginAction = new LoginImpl(SS);
+            
+//            Identify Action1 = new IdentifyImpl();  //create object based on action class
+            
+            reg.rebind("Login", LoginAction); 
             //basically adding this as an action client can find and use
             
             System.out.println("Server is now running... ");
