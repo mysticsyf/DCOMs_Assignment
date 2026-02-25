5f94ed28e59b6fcd95e77f38502cd24ae1e6d202
MelvinLMX <melvin.lee.mx@gmail.com>
2026-02-25 11:52:41 +0800

    Added temporary staff database .ser

--- DCOMs_Assignment/src/dcoms_assignment/ALImpl.java
+++ DCOMs_Assignment/src/dcoms_assignment/ALImpl.java
@@ -1,7 +1,7 @@
 package dcoms_assignment;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-//test
+//test 2
 public class ALImpl extends UnicastRemoteObject implements ApplyLeave{
     private LeaveService service;
     
