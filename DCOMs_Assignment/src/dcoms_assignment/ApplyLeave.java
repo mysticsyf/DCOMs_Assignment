d28c020e9171ec063e7f9b0d480302d95ba65f94
MelvinLMX <melvin.lee.mx@gmail.com>
2026-02-28 23:37:20 +0800

    changed parameters to better suit client

--- DCOMs_Assignment/src/dcoms_assignment/ApplyLeave.java
+++ DCOMs_Assignment/src/dcoms_assignment/ApplyLeave.java
@@ -1,11 +1,12 @@
 package dcoms_assignment;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.time.LocalDate;
 import java.util.Map;
 
 
 public interface ApplyLeave extends Remote{
-    String applyLeave(Leaves leave) throws RemoteException;
+    void applyLeave(Staff staff, LocalDate date1, LocalDate date2, String reason) throws RemoteException;
     String ApproveLeave(String leaveId) throws RemoteException;
     String RejectLeave(String leaveId) throws RemoteException;
     Map<String, Leaves> getAllLeaves() throws RemoteException;
