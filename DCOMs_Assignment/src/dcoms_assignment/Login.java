4d58b93e13e3c1b344520379129482840ae256ba
MelvinLMX <melvin.lee.mx@gmail.com>
2026-03-01 17:37:52 +0800

    New ViewProfile and UpdateProfile

--- DCOMs_Assignment/src/dcoms_assignment/Login.java
+++
@@ -1,8 +0,0 @@
-package dcoms_assignment;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Login extends Remote{
-    Staff Login(String StaffID, String password) throws RemoteException;
-}
