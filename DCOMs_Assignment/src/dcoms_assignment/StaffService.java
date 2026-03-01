1daf1a05c0bac0e8f2ace1b5ff31f0b9cacfd250
MelvinLMX <melvin.lee.mx@gmail.com>
2026-03-01 17:36:59 +0800

    New ViewProfile and UpdateProfile

--- DCOMs_Assignment/src/dcoms_assignment/StaffService.java
+++ DCOMs_Assignment/src/dcoms_assignment/StaffService.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StaffService {
+    
+    StringBuilder sb = new StringBuilder();
     private Map<String, Staff> StaffMap;
     private final String FileName = "Staff.ser";
 
@@ -25,6 +27,13 @@ public class StaffService {
             FileInputStream FileIn = new FileInputStream(FileName);
             ObjectInputStream In = new ObjectInputStream(FileIn);
             StaffMap = (HashMap<String, Staff>) In.readObject();
+            for(Staff staff : StaffMap.values()){
+                if(staff.getRole() == "STAFF"){
+                    StaffCounter += 1;
+                }else if(staff.getRole() == "HR"){
+                    HRCounter += 1;
+                }
+            }
             In.close();
             FileIn.close();
         } catch(IOException | ClassNotFoundException e){
@@ -45,8 +54,38 @@ public class StaffService {
         }
     }
     
-    public void AddStaff(Staff staff) {
-        StaffMap.put(staff.getStaffID(), staff);
+    private int StaffCounter = 1;
+    private int HRCounter = 1;
+    
+    private String generateStaffId(Staff staff) {
+        if(staff.getRole() == "STAFF"){
+            return String.format("ST%02d", StaffCounter++);
+        }else if(staff.getRole() == "HR"){
+            return String.format("HR%02d", HRCounter++);
+        }else{
+            return null;
+        }
+    }
+    
+    public void ReduceStaffLeave(String StaffID){
+        LoadfromFile();
+        Staff ChosenStaff = StaffMap.get(StaffID);
+        ChosenStaff.ReduceLeave();
+        SavetoFile();
+    }
+    
+    public void AddStaff(Staff staff){
+        String newId = generateStaffId(staff);
+        staff.setStaffID(newId);
+        StaffMap.put(newId, staff);
+        SavetoFile();
+    }
+    
+    public void AddStaff(String name, String role, int salary){
+        Staff NewStaff = new Staff("", name, role, "1234", salary);
+        String newId = generateStaffId(NewStaff);
+        NewStaff.setStaffID(newId);
+        StaffMap.put(newId, NewStaff);
         SavetoFile();
     }
     
@@ -64,9 +103,68 @@ public class StaffService {
         }
     }
     
-    public Map<String, Staff> getAllStaff(){
-        return StaffMap;
+    public String ViewProfile(Staff staff){
+        Staff StaffShow = StaffMap.get(staff.getStaffID());
+        return StaffShow.toString();
     }
     
-    //add more find staff stuff
+    public String UpdateName(String ChosenID, String newName){
+        LoadfromFile();
+        Staff ChosenStaff = StaffMap.get(ChosenID);
+        ChosenStaff.setName(newName);
+        SavetoFile();
+        return "Name: " + newName + "has been updated";
+    }
+    
+    public String UpdateRole(String ChosenID, String newRole){
+        LoadfromFile();
+        Staff ChosenStaff = StaffMap.get(ChosenID);
+        ChosenStaff.setRole(newRole);
+        SavetoFile();
+        return "Role: " + newRole + "has been updated";
+    }
+    
+    public String UpdatePassword(String ChosenID, String newPassword){
+        LoadfromFile();
+        Staff ChosenStaff = StaffMap.get(ChosenID);
+        ChosenStaff.setPassword(newPassword);
+        SavetoFile();
+        return "Password: " + newPassword + "has been updated";
+    }
+    
+    public String UpdateSalary(String ChosenID, int newSalary){
+        LoadfromFile();
+        Staff ChosenStaff = StaffMap.get(ChosenID);
+        ChosenStaff.setSalary(newSalary);
+        SavetoFile();
+        return "Salary: " + newSalary + "has been updated";
+    }
+    
+    public String UpdateLeaves(String ChosenID, int newRemLeave){
+        LoadfromFile();
+        Staff ChosenStaff = StaffMap.get(ChosenID);
+        ChosenStaff.setRemainingLeaves(newRemLeave);
+        SavetoFile();
+        return "Remaining Leaves: " + newRemLeave + "has been updated";
+    }
+    
+    public String getAllStaff() {
+
+        LoadfromFile();
+
+        if (StaffMap.isEmpty()) {
+            return "No staff records found.";
+        }
+        for (Staff staff : StaffMap.values()) {
+            sb.append("----------------------------\n");
+            sb.append("Staff ID: ").append(staff.getStaffID()).append("\n");
+            sb.append("Name: ").append(staff.getName()).append("\n");
+            sb.append("Role: ").append(staff.getRole()).append("\n");
+            sb.append("Password: ").append(staff.getPassword()).append("\n");
+            sb.append("Salary: ").append(staff.getSalary()).append("\n");
+            sb.append("Remaining Leaves").append(staff.getRemainingLeaves()).append("\n");
+        }
+        sb.append("\nSelect StaffID: ");
+        return sb.toString();
+    }
 }
